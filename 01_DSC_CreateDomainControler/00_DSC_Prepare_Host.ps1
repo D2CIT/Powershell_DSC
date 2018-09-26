eeea0bbe7003abe786a90349f790a29d9688a6e7
Mark <Mark@d2c-it.nl>
2018-09-26 17:39:52 +0200

    First Commit

---
+++ 01_DSC_CreateDomainControler/00_DSC_Prepare_Host.ps1
@@ -0,0 +1,114 @@
+﻿
+################################################################################
+# ██████╗ ██████╗  ██████╗       ██╗████████╗
+# ██╔══██╗╚════██╗██╔════╝       ██║╚══██╔══╝
+# ██║  ██║ █████╔╝██      ║█████╗██║   ██║   
+# ██║  ██║██╔═══╝ ██║     ╚════ ╝██║   ██║   
+# ██████╔╝███████╗╚██████╗       ██║   ██║   
+# ╚═════╝ ╚══════╝ ╚═════╝       ╚═╝   ╚═╝   
+################################################################################
+# Prepare Host for DSC
+# ------------------------------------------------------------------------------
+#    Author  : Mark van de Waarsenburg (Netherlands)
+#    Company : D2C-IT  - Dare to Change IT
+#    Date    : 29-9-2018
+#    Script  : .\00_DSC_Prepare_Host.ps1
+#
+################################################################################
+
+<#
+.Synopsis
+   Prepare Server For DSC Config and start DSC
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Prepare and rename Host
+   .\00_DSC_Prepare_Host.ps1 -newname LABDC01
+#>
+
+
+Param(
+    #Param 1 : default
+    [Parameter(Mandatory=$false)]  
+    [String]$scriptfolder = "c:\Scripts"  , 
+    
+    #Param 2 : only use when tyou want to rename your host. Host wil reboot automaticly
+    [Parameter(Mandatory=$false)]      
+    [String]$NewName ,  
+     #Param 3 :  If you als want install devoptools lile Git,Visualcode etc. 
+    [Parameter(Mandatory=$false)]  
+    [switch]$IncludeDevOPTools 
+)
+
+Begin{
+
+    Function prep-DSCforHost       {
+
+        Param($scriptfolder)
+ 
+        # CREATE LOCAL SCRIPT FOLDER      
+            If(!(test-path $scriptfolder)){mkdir $scriptfolder | out-null}
+            If(!(test-path $scriptfolder\Powershell)){mkdir $scriptfolder\Powershell | out-null}
+            If(!(test-path $scriptfolder\Powershell\Cert)){mkdir $scriptfolder\Powershell\Cert | out-null ;  Write-host "[Note] : create Folder $scriptfolder with Sub directory's"  -for Green}
+            If(!(test-path $scriptfolder\Powershell\DSC)){mkdir $scriptfolder\Powershell\DSC  | out-null ;  Write-host "[Note] : create Folder $scriptfolder with Sub directory's"  -for Green}
+            cd $scriptfolder
+
+        # SETUP PACKAGE PROVIDER
+            If( get-PackageProvider | where {$_.name -like "nuget"}){
+                Write-host "[Note] : PackageProvider Nuget is installed"  -for Green
+            }Else{
+                Write-host "[Note] : Could not find PackageProvider Nuget and will be installed"  -for Yellow
+                # Setup packageSource
+                Get-PackageSource -name PSGallery | set-PackageSource -trusted -Force -ForceBootstrap | out-null
+                Install-PackageProvider -name NuGet -force -Confirm:$false| out-null
+            }#EndIF
+
+        # ENABLE PSREMOTING 
+            #write-host "[Note] : enable PSremoting " -for Yellow
+            #enable-psremoting -Force
+            #wsman quickconfig
+
+        # IMPORT DSC RESOURCES
+            write-host "[Note] : Install Powershell Modules for DSC" -for Yellow
+            $modules = @("xComputerManagement","xNetworking","xDnsServer","xActiveDirectory","xPSDesiredStateConfiguration","cFileShare","xWinRM","xTimeZone","xWinEventLog" )
+            Foreach($Module in $modules){
+                write-host "         - Module $module " -for Yellow
+                Install-Module -Name $Module 
+            }
+      
+    } #End Function
+
+}
+
+Process{
+    # PREPARE HOST FOR DSC
+    prep-DSCforHost -scriptfolder $scriptfolder
+
+    # INSTALL DEVOPS TOOLING
+    If($IncludeDevOPTools){
+        #setup Scripting Apps for Powershell/GIT
+        If(!(Test-Path -Path "$env:ProgramData\Chocolatey")){
+            Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+            choco upgrade chocolatey
+            choco feature enable -n allowGlobalConfirmation
+        }
+
+        $Packages = 'win32-openssh', 'git', 'notepadplusplus', 'poshgit', 'visualstudiocode'
+
+        ForEach ($PackageName in $Packages){
+                write-host "[note] : Install $packagename" -for Yellow
+                $x = choco install $PackageName -y 
+                write-host "         - $($x[4])" -for Green
+        }#EndForeach  
+            
+    }#EndIF
+
+    # Rename Host
+    If($NewName){
+        if(!($env:computername -eq $newName)){
+            Write-host "[note] : rename host from $($env:computername) to $NewName"
+            rename-computer -newname $NewName -Restart
+        }
+    }#EndIF
+
+}
---
+++ 01_DSC_CreateDomainControler/01_DSC_CreateDomainController_Secure.ps1
@@ -0,0 +1,358 @@
+
+################################################################################
+# ██████╗ ██████╗  ██████╗       ██╗████████╗
+# ██╔══██╗╚════██╗██╔════╝       ██║╚══██╔══╝
+# ██║  ██║ █████╔╝██      ║█████╗██║   ██║   
+# ██║  ██║██╔═══╝ ██║     ╚════ ╝██║   ██║   
+# ██████╔╝███████╗╚██████╗       ██║   ██║   
+# ╚═════╝ ╚══════╝ ╚═════╝       ╚═╝   ╚═╝   
+################################################################################
+# Configure server as Domain Controller (PUSH)
+# ------------------------------------------------------------------------------
+#    Author  : Mark van de Waarsenburg (Netherlands)
+#    Company : D2C-IT  - Dare to Change IT
+#    Date    : 29-9-2018
+#    Script  : .\00_DSC_CreateDomainController.ps1
+#    Note    : First run .\00_DSC_Prepare_Host.ps1
+#
+################################################################################
+<#
+.Synopsis
+   Create domaincontroller via DSC
+.DESCRIPTION
+   Create domaincontroller via DSC
+.EXAMPLE
+   Example of how to use this cmdlet
+.Note
+   Last Edit : 26-09-2018
+#>
+Param(
+
+    [Parameter(Mandatory=$false)]                       
+    [string]$ComputerName      = "labdc01" ,                         
+    
+    [Parameter(Mandatory=$false)]  
+    [string]$IPAddress         = "192.168.16.140/24",  
+    
+    [Parameter(Mandatory=$false)]               
+    [array]$DnsIpAddress       = @("192.168.16.140","192.168.16.2") , 
+    
+    [Parameter(Mandatory=$false)]
+    [string]$GatewayAddress    = "192.168.16.2", 
+    
+    [Parameter(Mandatory=$false)]                   
+    [string]$InterfaceAlias    = "Ethernet0",
+    
+    [Parameter(Mandatory=$false)]                  
+    [string]$DomainName        = "d2cit.it" ,
+    
+    [Parameter(Mandatory=$false)]                     
+    [string]$CertificateFile   = "C:\scripts\Powershell\Cert\DscPublicKey.cer" , 
+    
+    [Parameter(Mandatory=$false)]
+    [string]$DomainUsername    = "d2cit\administrator",
+    
+    [Parameter(Mandatory=$false)]
+    [string]$DomainDN          = "DC=d2cit,DC=it" , 
+      
+    [Parameter(Mandatory=$false)]
+    [string]$scriptfolder      = "c:\Scripts", 
+    
+    [switch]$ForceCreateSelfSignedCertificate,
+    [Switch]$Reboot
+    
+)
+
+Begin{
+  
+    ##############################################################
+    #  DSC CONFIG
+    ##############################################################
+    configuration BuildDomainController  {
+
+        Import-DscResource -ModuleName xActiveDirectory, xComputerManagement, xNetworking, xDnsServer, PSDesiredStateConfiguration
+
+        Node localhost
+        {
+            LocalConfigurationManager {
+                ActionAfterReboot = 'ContinueConfiguration'
+                ConfigurationMode = 'ApplyOnly'
+                RebootNodeIfNeeded = $true
+            }
+
+            ###########################################################################
+            # Set IPAdress , Local Admin Password and Hostname
+            ###########################################################################  
+            xIPAddress NewIPAddress {
+                IPAddress = $node.IPAddress
+                InterfaceAlias = $node.InterfaceAlias
+                AddressFamily = 'IPV4'
+            }
+            xDefaultGatewayAddress NewIPGateway {
+                Address = $node.GatewayAddress
+                InterfaceAlias = $node.InterfaceAlias
+                AddressFamily = 'IPV4'
+                DependsOn = '[xIPAddress]NewIPAddress'
+            }
+            xDnsServerAddress PrimaryDNSClient {
+                Address = $node.DnsAddress
+                InterfaceAlias = $node.InterfaceAlias
+                AddressFamily = 'IPV4'
+                DependsOn = '[xDefaultGatewayAddress]NewIPGateway'
+            }
+
+            User Administrator {
+                Ensure    = 'Present'
+                UserName  = 'Administrator'
+                Password  = $domainCred
+                DependsOn = '[xDnsServerAddress]PrimaryDNSClient'
+            }
+            xComputer NewComputerName {
+                Name = $node.ThisComputerName
+                DependsOn = '[User]Administrator'
+            }
+
+            ###########################################################################
+            # Install Windows features
+            ###########################################################################  
+            WindowsFeature ADDSInstall {
+                Ensure = 'Present'
+                Name = 'AD-Domain-Services'
+                DependsOn = '[xComputer]NewComputerName'
+            }
+
+            ###########################################################################
+            # Create First Domain
+            ###########################################################################
+            xADDomain FirstDC {
+                DomainName = $node.DomainName
+                DomainAdministratorCredential = $domainCred
+                SafemodeAdministratorPassword = $domainCred
+                DatabasePath = $node.DCDatabasePath
+                LogPath = $node.DCLogPath
+                SysvolPath = $node.SysvolPath 
+                DependsOn = '[WindowsFeature]ADDSInstall'
+            }
+
+            ###########################################################################
+            # Create default OU structure
+            ###########################################################################
+            xADOrganizationalUnit D2C {
+                Name = "D2C"  
+                Path = "$($node.DomainDN)"          
+                Description = "Root OU D2C"
+                ProtectedFromAccidentalDeletion = $True
+                DependsOn = '[xADDomain]FirstDC'
+            }
+
+            xADOrganizationalUnit Servers {
+                Path = "OU=D2C,$($node.DomainDN)"
+                Name = "Servers"            
+                Description = "Server OU"
+                ProtectedFromAccidentalDeletion = $True
+                DependsOn = '[xADOrganizationalUnit]D2C'
+            }
+            xADOrganizationalUnit computers {
+                Path = "OU=D2C,$($node.DomainDN)"
+                Name = "computers"            
+                Description = "Computer OU"
+                ProtectedFromAccidentalDeletion = $True
+                DependsOn = '[xADOrganizationalUnit]D2C'
+            }
+            xADOrganizationalUnit Users {
+                Path = "OU=D2C,$($node.DomainDN)"
+                Name = "Users"            
+                Description = "users OU"
+                ProtectedFromAccidentalDeletion = $True
+                DependsOn = '[xADOrganizationalUnit]D2C'
+            }
+            xADOrganizationalUnit groups {
+                Path = "OU=D2C,$($node.DomainDN)"
+                Name = "Groups"            
+                Description = "Groups OU"
+                ProtectedFromAccidentalDeletion = $True
+                DependsOn = '[xADOrganizationalUnit]D2C'
+            }
+        
+            ###########################################################################
+            # Create Users and add to the correct OU's
+            ###########################################################################
+            xADUser Adminmw {
+                DomainName = $node.DomainName
+                Path = "ou=Users,ou=D2C,$($node.DomainDN)"
+                UserName = 'Adminmw'
+                GivenName = 'Mark'
+                Surname = 'van de Waarsenburg'
+                DisplayName = 'Mark van de Waarsenburg'
+                Enabled = $true
+                Password = $domaincred
+                DomainAdministratorCredential = $domainCred
+                PasswordNeverExpires = $true
+                DependsOn = '[xADOrganizationalUnit]Users'
+            }
+            xADUser AdminJM {
+                DomainName = $node.DomainName
+                Path = "ou=Users,ou=D2C,$($node.DomainDN)"
+                UserName = 'AdminJM'
+                GivenName = 'Jean-Marc'
+                Surname = 'Zohlandt'
+                DisplayName = 'Jean-Marc Zohlandt'
+                Enabled = $true
+                Password = $domaincred
+                DomainAdministratorCredential = $domainCred
+                PasswordNeverExpires = $true
+                DependsOn = '[xADOrganizationalUnit]Users'
+            }
+            xADUser AdminDH {
+                DomainName = $node.DomainName
+                Path = "ou=Users,ou=D2C,$($node.DomainDN)"
+                UserName = 'AdminDH'
+                GivenName = 'Donna'
+                Surname = 'Danny'
+                DisplayName = 'Hoeke'
+                Enabled = $true
+                Password = $domaincred
+                DomainAdministratorCredential = $domainCred
+                PasswordNeverExpires = $true
+                DependsOn = '[xADOrganizationalUnit]Users'
+            }
+            xADUser AdminMH {
+                DomainName = $node.DomainName
+                Path = "OU=Users,OU=D2C,$($node.DomainDN)"
+                UserName = 'AdminMH'
+                GivenName = 'Martijn'
+                Surname = 'Haverhoek'
+                DisplayName = 'Martijn Haverhoek'
+                Enabled = $true
+                Password = $domaincred
+                DomainAdministratorCredential = $domainCred
+                PasswordNeverExpires = $true
+                DependsOn = '[xADOrganizationalUnit]Users'
+            }
+            ###########################################################################
+            # Create and configure Groups
+            ###########################################################################
+            xADGroup IT {
+                GroupName = 'IT'
+                Path = "OU=Groups,OU=D2C,$($node.DomainDN)"
+                Category = 'Security'
+                GroupScope = 'Global'
+                MembersToInclude = 'AdminMH', 'AdminJM', 'AdminMW', 'AdminDH'
+                DependsOn = '[xADOrganizationalUnit]groups'
+            }
+            xADGroup DomainAdmins {
+                GroupName = 'Domain Admins'
+                Path = "CN=Users,$($node.DomainDN)"
+                Category = 'Security'
+                GroupScope = 'Global'
+                MembersToInclude = 'AdminJM', 'AdminMW'
+                DependsOn = '[xADDomain]FirstDC'
+            }
+            xADGroup EnterpriseAdmins {
+                GroupName = 'Enterprise Admins'
+                Path = "CN=Users,$($node.DomainDN)"
+                Category = 'Security'
+                GroupScope = 'Universal'
+                MembersToInclude = 'AdminMW'
+                DependsOn = '[xADDomain]FirstDC'
+            }
+            xADGroup SchemaAdmins {
+                GroupName = 'Schema Admins'
+                Path = "CN=Users,$($node.DomainDN)"
+                Category = 'Security'
+                GroupScope = 'Universal'
+                MembersToInclude = 'AdminMW'
+                DependsOn = '[xADDomain]FirstDC'
+            }
+            xDnsServerADZone addReverseADZone {
+                Name = '16.168.192.in-addr.arpa'
+                DynamicUpdate = 'Secure'
+                ReplicationScope = 'Forest'
+                Ensure = 'Present'
+                DependsOn = '[xADDomain]FirstDC'
+            }
+        }#Node
+    }#configuration
+    Configuration lcmconfigSecure        {
+        #parameters
+        param(
+            [string[]]$computername,
+            $CertificateID
+        )
+
+        #Target Node
+        Node $computername {
+            LocalconfigurationManager {
+                ConfigurationMode              = "applyAndAutocorrect"
+                ConfigurationModeFrequencyMins = 15
+                CertificateID                  = $CertificateID
+                RefreshMode                    = "Push"
+                rebootNodeIfNeeded             = $true
+            }
+        }
+     } #End PushedConfig
+
+    # CREATE LOCAL SELFSIGNEDCERTIFICATE    
+    if( !(test-path "$scriptfolder\Powershell\Cert\DscPublickey.cer") -or $ForceCreateSelfSignedCertificate){
+        $cert = New-SelfSignedCertificate -Type DocumentEncryptionCertLegacyCsp `
+                                            -DnsName "DSCEncryptionCert_$($env:computername)" `
+                                            -FriendlyName "Server Authentication" `
+                                            -HashAlgorithm SHA256 
+        $cert | Export-Certificate -FilePath $CertificateFile -Force
+    }
+    
+
+}
+
+Process {   
+    #Change Directory to store DSC MOF Files 
+    If(!(test-path $scriptfolder\powershell\dsc)){}
+        set-location $scriptfolder\powershell\dsc
+    }
+
+    #Import Certificate
+    $ImpCert = Import-Certificate -filepath $CertificateFile -CertStoreLocation cert:\localmachine\my
+
+    # Create MOF LCM With Hashed Password in MOF         
+    lcmconfigSecure -CertificateID $ImpCert.Thumbprint
+
+
+    # Set paramaters for Config    
+    $ConfigDataSecure = @{
+        AllNodes = @(
+            @{
+                Nodename                    = "localhost"
+                ThisComputerName            = $ComputerName
+                IPAddress                   = $IPAddress 
+                DnsAddress                  = $DnsIpAddress
+                GatewayAddress              = $GatewayAddress
+                InterfaceAlias              = $InterfaceAlias
+                DomainName                  = $DomainName
+                DomainDN                    = $DomainDN
+                DCDatabasePath              = "C:\NTDS"
+                DCLogPath                   = "C:\NTDS"
+                SysvolPath                  = "C:\Sysvol"
+                PSDscAllowPlainTextPassword = $false
+                PSDscAllowDomainUser        = $true
+                CertificateFile             = $CertificateFile
+                Thumbprint                  = $ImpCert.Thumbprint
+            }
+        )#AllNodes
+    }#ConfigData
+
+    # Set Password
+    $domainCred = Get-Credential -UserName $DomainUsername -Message "Please enter a new password for Domain Administrator."
+
+    # Build MOF File
+    BuildDomainController -ConfigurationData $ConfigDataSecure
+   
+    # Set LCM Manager
+    Set-DscLocalConfigurationManager -path lcmconfigSecure -verbose
+
+    # Push Configuration
+    Start-DscConfiguration -Wait -Force -Path .\BuildDomainController -Verbose
+}
+
+End{
+    #Script Finished
+}
